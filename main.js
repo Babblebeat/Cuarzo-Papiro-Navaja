bfb98528d64b8673f59c2087a4d15364eb5a9c9c
shiroda <47889808+shirodavid@users.noreply.github.com>
2020-08-21 10:43:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
 
-function human(num) {
-    var x = num;
-    var count_hum
-    switch (x) {
-        case 1:
-            count_hum = 1;
-
-            ph1 = ("Escojes Cuarzo")
-
-            break;
-
-        case 2:
-            count_hum = 2;
-
-            ph1 = ("Escojes Papiro")
-            break;
-
-        case 3:
-            count_hum = 3;
-            ph1 = ("Escojes Navaja")
-            break;
-    }
-    var final_1 = Number(count_hum);
-    document.getElementById("human_say").innerHTML = final_1;
-    document.getElementById("human").innerHTML = ph1;
-
-}
-
 ///////////////////////////////////////////////////////////////////
 function machine(num) {
     var count_hum = num;
@@ -34,48 +6,66 @@ function machine(num) {
 
     switch (count_bot) {
         case 1:
-            ph2 = ("El bot escoje Cuarzo, ");
+            second_string = ("El bot escoje Cuarzo: ");
             break;
         case 2:
-            ph2 = ("El bot escoje Papiro, ");
+            second_string = ("El bot escoje Papiro: ");
             break;
 
         case 3:
-            ph2 = ("El bot escoje Navaja, ");
+            second_string = ("El bot escoje Navaja: ");
             break;
     }
 
     if (count_hum == 3 && count_bot == 1) {
-        ph3 = ("Tú pierdes.")
+        third_string = ("Tú pierdes.")
 
     } else if (count_hum == 1 && count_bot == 3) {
-        ph3 = ("Tú ganas.")
+        third_string = ("Tú ganas.")
 
     } else if (count_hum < count_bot) {
-        ph3 = ("Tú pierdes.")
+        third_string = ("Tú pierdes.")
 
     } else if (count_hum > count_bot) {
-        ph3 = ("Tú ganas.")
+        third_string = ("Tú ganas.")
 
     } else if (count_hum == count_bot) {
-        ph3 = ("Empate.")
+        third_string = ("Empate.")
 
     }
-    var machine_say = ph2 + ph3;
+    var machine_say = second_string + third_string;
     return machine_say;
 }
 //////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
-function game() {
-
-
-    var count_hum = document.getElementById("human_say").innerHTML;
+function game(num) {
     
-    document.getElementById("bot").innerHTML = machine(count_hum);
-}
+    var x = num;
+    var count_hum
+    switch (x) {
+        case 1:
+            count_hum = 1;
+            first_string = ("Escojes Cuarzo, ")
+            break;
 
+        case 2:
+            count_hum = 2;
+            first_string = ("Escojes Papiro, ")
+            break;
+
+        case 3:
+            count_hum = 3;
+            first_string = ("Escojes Navaja; ")
+            break;
+    }
+    var final_1 = Number(count_hum);
+    var final_2 = String(first_string);
+    
+    document.getElementById("bot").innerHTML = final_2 + machine(final_1);
+}
+/////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 function reset() {
     document.getElementById("bot").innerHTML = "_";
 }
-
